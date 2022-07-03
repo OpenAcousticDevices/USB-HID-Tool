0a27e4f88cb8d8c7c277be6e2f24ce94f2dd012d
Chris Couzens <ccouzens@gmail.com>
2022-07-03 23:41:40 +0100

    Place the executable in the destination immediately
    Before when it copied, it didn't copy the executable bit. Therefore
    adding the executable bit was a second step.
    
    GCC puts it on by default, so having gcc build it in the right directory
    ensures the binary has the executable bit.

--- src/linux/build.sh
+++ src/linux/build.sh
@@ -1,5 +1,1 @@
-gcc -Wall -std=c99 -I/usr/include/libusb-1.0 -I.. -o usbhidtool ../main.c hid.c -lusb-1.0 -lrt -lpthread
-
-cp usbhidtool ../../bin/linux/usbhidtool
-
-rm -f usbhidtool
+gcc -Wall -std=c99 -I/usr/include/libusb-1.0 -I.. -o ../../bin/linux/usbhidtool ../main.c hid.c -lusb-1.0 -lrt -lpthread
